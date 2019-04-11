ecccaa4bf95c73082be7f12ef5b14c404412aa05
mudkipmaster <acg59@drexel.edu>
2019-04-11 13:06:58 -0400

    Added tests and fixed a bug

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,20 +1,25 @@
 import unittest
 from versioned_classes import versioned_class
 
-class TestSum(unittest.TestCase):
+class TestVersionedClasses(unittest.TestCase):
 
     @versioned_class
     class TestClass1():
         def testMethod(self):
             return "hello"
 
+
     @versioned_class
     class TestClass2():
         def testMethod(self):
             return "goodbye"
 
-    def test_equal(self):
-        self.assertEqual(self.TestClass1.__version__ , self.TestClass1.__version__)
+    def test_equal(self): #if two classes have the same code
+        sut = self.TestClass1()
+        sut2 = self.TestClass1()
+        self.assertEqual(sut.__version__ , sut2.__version__)
 
-    def test_not_equal(self):
-        self.assertEqual(self.TestClass1.__version__, self.TestClass2.__version__)
+    def test_not_equal(self): #if two classes have different code
+        sut = self.TestClass1()
+        sut2 = self.TestClass2()
+        self.assertNotEqual(sut.__version__, sut2.__version__)
--- versioned_classes/__init__.py
+++ versioned_classes/__init__.py
@@ -1,11 +1,19 @@
 import hashlib
 import inspect
+import types
 
 
 def versioned_class(cls):
-    m = hashlib.md5()
-    for methodname, method in inspect.getmembers(cls):
-        if inspect.ismethod(method):
-            m.update(inspect.getsource(method).encode("utf-8"))
-    cls.__version__ = m.hexdigest()
-    return cls
+    """
+    This decorator creates a hash of the code inside a classes method and saves it as cls.__version__
+    """
+    class Wrapper(cls):
+        def __init__(self,*args):
+            super().__init__(*args)
+            m = hashlib.md5()
+            for methodname, method in vars(cls).items():
+                if isinstance(method, types.FunctionType):
+                    m.update(inspect.getsource(method).encode("utf-8"))
+            self.__version__ = m.hexdigest()
+            print(self.__version__)
+    return Wrapper
